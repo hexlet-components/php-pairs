c1b6934add2c888b09d10f0435be69011011f68c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-01-12 13:04:08 +0200

    update docs (#5)

--- src/Pairs.php
+++ src/Pairs.php
@@ -2,6 +2,12 @@
 
 namespace Pairs;
 
+/**
+ * Creates a pair with two values
+ * @param mixed $x first value
+ * @param mixed $y second value
+ * @return callable pair
+ */
 function cons($x, $y)
 {
     return function ($method) use ($x, $y) {
@@ -16,16 +22,31 @@ function cons($x, $y)
     };
 }
 
+/**
+ * Extracts first value from pair
+ * @param callalble $pair
+ * @return mixed
+ */
 function car(callable $pair)
 {
     return $pair("car");
 }
 
+/**
+ * Extracts second value from pair
+ * @param callalble $pair
+ * @return mixed
+ */
 function cdr(callable $pair)
 {
     return $pair("cdr");
 }
 
+/**
+ * Converts given list to string
+ * @param callalble $list
+ * @return string
+ */
 function toString($list)
 {
     if (!isPair($list)) {
@@ -43,6 +64,11 @@ function toString($list)
     return "(" . implode(", ", $arr) . ")";
 }
 
+/**
+ * Checks whether given $pair is valid pair
+ * @param callable $pair
+ * @return boolean
+ */
 function isPair($pair)
 {
     return is_callable($pair);
